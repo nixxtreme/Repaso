a09e7a80981d0ec0fe9ebc309cac103175f813b4
Yosh <nixxtreme@gmail.com>
2025-03-08 23:03:30 -0600

    Se actualiza versión a clase del 8 de marzo

--- src/Suma.java
+++ src/Suma.java
@@ -1,6 +1,11 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Suma extends JFrame{
     private JPanel panel1;
@@ -21,6 +26,8 @@ public class Suma extends JFrame{
     private JButton btnpnt;
     private JButton btndiv;
     private JButton btnequal;
+    private JButton CEButton;
+    private JButton DelButton;
     double operando1;
     double operando2;
     double result;
@@ -90,47 +97,59 @@ public class Suma extends JFrame{
                 textPantalla.setText(textPantalla.getText() +  "0");
             }
         });
+
+
         btnpnt.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+/*
                 if(!textPantalla.getText().contains(".")){
                     textPantalla.setText(textPantalla.getText() +  ".");
                 }
+
+ */
+                String array[] = textPantalla.getText().split("[*/+\\-]");
+                /*if(array.length == 1){
+                    if(!array[0].contains(".")){
+                        textPantalla.setText(textPantalla.getText() +  ".");
+                    }
+                }else{
+                    if(!array[1].contains(".")) {
+                        textPantalla.setText(textPantalla.getText() + ".");
+                    }
+                }*/
+                if(!array[array.length-1].contains(".")){
+                    textPantalla.setText(textPantalla.getText() + ".");
+                }
+
             }
         });
         btndiv.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if((!textPantalla.getText().contains("/") &&
-                        !textPantalla.getText().contains("/") &&
-                        !textPantalla.getText().contains("-") &&
-                        !textPantalla.getText().contains("+")
-                ) && (
+                if(
                         textPantalla.getText().contains("0") ||
-                                textPantalla.getText().contains("1") ||
-                                textPantalla.getText().contains("2") ||
-                                textPantalla.getText().contains("3") ||
-                                textPantalla.getText().contains("4") ||
-                                textPantalla.getText().contains("5") ||
-                                textPantalla.getText().contains("6") ||
-                                textPantalla.getText().contains("7") ||
-                                textPantalla.getText().contains("8") ||
-                                textPantalla.getText().contains("9")
-                )){
+                        textPantalla.getText().contains("1") ||
+                        textPantalla.getText().contains("2") ||
+                        textPantalla.getText().contains("3") ||
+                        textPantalla.getText().contains("4") ||
+                        textPantalla.getText().contains("5") ||
+                        textPantalla.getText().contains("6") ||
+                        textPantalla.getText().contains("7") ||
+                        textPantalla.getText().contains("8") ||
+                        textPantalla.getText().contains("9")
+                ){
                     textPantalla.setText(textPantalla.getText() + "/");
                 }
+
+
             }
         });
 
         btnprod.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if((!textPantalla.getText().contains("*") &&
-                        !textPantalla.getText().contains("/") &&
-                        !textPantalla.getText().contains("-") &&
-                        !textPantalla.getText().contains("+")
-                ) && (
+                if(
                         textPantalla.getText().contains("0") ||
                                 textPantalla.getText().contains("1") ||
                                 textPantalla.getText().contains("2") ||
@@ -141,7 +160,7 @@ public class Suma extends JFrame{
                                 textPantalla.getText().contains("7") ||
                                 textPantalla.getText().contains("8") ||
                                 textPantalla.getText().contains("9")
-                )){
+                ){
                     textPantalla.setText(textPantalla.getText() + "*");
                 }
             }
@@ -149,11 +168,7 @@ public class Suma extends JFrame{
         btnsum.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if((!textPantalla.getText().contains("*") &&
-                        !textPantalla.getText().contains("/") &&
-                        !textPantalla.getText().contains("-") &&
-                        !textPantalla.getText().contains("+")
-                ) && (
+                if(
                         textPantalla.getText().contains("0") ||
                                 textPantalla.getText().contains("1") ||
                                 textPantalla.getText().contains("2") ||
@@ -164,7 +179,7 @@ public class Suma extends JFrame{
                                 textPantalla.getText().contains("7") ||
                                 textPantalla.getText().contains("8") ||
                                 textPantalla.getText().contains("9")
-                )){
+                ){
                     textPantalla.setText(textPantalla.getText() + "+");
                 }
             }
@@ -173,11 +188,7 @@ public class Suma extends JFrame{
         btnres.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if((!textPantalla.getText().contains("*") &&
-                        !textPantalla.getText().contains("/") &&
-                        !textPantalla.getText().contains("-") &&
-                        !textPantalla.getText().contains("+")
-                ) && (
+                if(
                         textPantalla.getText().contains("0") ||
                                 textPantalla.getText().contains("1") ||
                                 textPantalla.getText().contains("2") ||
@@ -188,7 +199,7 @@ public class Suma extends JFrame{
                                 textPantalla.getText().contains("7") ||
                                 textPantalla.getText().contains("8") ||
                                 textPantalla.getText().contains("9")
-                )){
+                ){
                     textPantalla.setText(textPantalla.getText() + "-");
                 }
             }
@@ -197,37 +208,107 @@ public class Suma extends JFrame{
         btnequal.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(textPantalla.getText().contains("/")){
+                /*if(textPantalla.getText().contains("/")){
                     String array[] = textPantalla.getText().split("/");
                     operando1 = Float.valueOf(array[0]);
                     operando2 = Float.valueOf(array[1]);
                     if(operando2 != 0){
                         result = operando1 / operando2;
-                        textPantalla.setText(textPantalla.getText() + " = " + result);
+                        textPantalla.setText(String.valueOf(result));
                     }else{
                         textPantalla.setText("Error");
                     }
-
                 }
                 if(textPantalla.getText().contains("*")){
-                    String array[] = textPantalla.getText().split("*");
+                    String array[] = textPantalla.getText().split("\\*");
                     operando1 = Float.valueOf(array[0]);
                     operando2 = Float.valueOf(array[1]);
+                    result = operando1 * operando2;
+                    textPantalla.setText(String.valueOf(result));
                 }
                 if(textPantalla.getText().contains("+")){
-                    String array[] = textPantalla.getText().split("+");
+                    String array[] = textPantalla.getText().split("\\+");
                     operando1 = Float.valueOf(array[0]);
                     operando2 = Float.valueOf(array[1]);
+                    result = operando1 + operando2;
+                    textPantalla.setText(String.valueOf(result));
                 }
                 if(textPantalla.getText().contains("-")){
                     String array[] = textPantalla.getText().split("-");
                     operando1 = Float.valueOf(array[0]);
                     operando2 = Float.valueOf(array[1]);
+                    result = operando1 - operando2;
+                    textPantalla.setText(String.valueOf(result));
+                }*/
+
+                int opera;
+                double resParc = 0.0;
+                List<String> numeros = new ArrayList<>();
+
+                String nums[] = textPantalla.getText().split("[*/+\\-]");
+                //numeros = Arrays.stream(nums).toList();
+                numeros = new ArrayList<>(Arrays.asList(nums));
+                List<String> operadores = new ArrayList<>();
+                Pattern pattern = Pattern.compile("[*/+\\-]");
+                Matcher matcher = pattern.matcher((textPantalla.getText()));
+                while (matcher.find()) {
+                    operadores.add(matcher.group());
                 }
+
+                while(!operadores.isEmpty()){
+                    while(operadores.indexOf("*") != -1){
+                        operar(operadores, numeros, operadores.indexOf("*"));
+                    }
+                    while(operadores.indexOf("/") != -1){
+                        operar(operadores, numeros, operadores.indexOf("/"));
+                    }
+                    while(operadores.indexOf("+") != -1){
+                        operar(operadores, numeros, operadores.indexOf("+"));
+                    }
+                    while(operadores.indexOf("-") != -1){
+                        operar(operadores, numeros, operadores.indexOf("-"));
+                    }
+                }
+                textPantalla.setText(String.valueOf(numeros.get(0)));
 
             }
         });
-    }
 
+        CEButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textPantalla.setText("");
+            }
+        });
+
+        DelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textPantalla.setText(textPantalla.getText().substring(0, textPantalla.getText().length()-1));
+            }
+        });
+    }
+    void operar(List<String> operadores, List<String> numeros, int opera){
+        Double resParc = 0.0;
+        switch(operadores.get(opera)){
+            case "+":
+                resParc = Double.valueOf(numeros.get(opera)) + Double.valueOf(numeros.get(opera + 1));
+                break;
+            case "-":
+                resParc = Double.valueOf(numeros.get(opera)) - Double.valueOf(numeros.get(opera + 1));
+                break;
+            case "*":
+                resParc = Double.valueOf(numeros.get(opera)) * Double.valueOf(numeros.get(opera + 1));
+                break;
+            case "/":
+                resParc = Double.valueOf(numeros.get(opera)) / Double.valueOf(numeros.get(opera + 1));
+                break;
+        }
+
+        numeros.remove(opera);
+        numeros.remove(opera);
+        numeros.add(opera, String.valueOf(resParc));
+        operadores.remove(opera);
+    }
 
 }
